d1b2687c9f29690c101baf7a6143cb1e3227d084
Rlzee <romainprovencel@icloud.com>
2025-11-17 18:00:18 +0100

    feat: update Input component styles and add color variables for input handling

--- components/input.tsx
+++ components/input.tsx
@@ -11,7 +11,7 @@ const Input = ({
       type={type}
       data-slot="input"
       className={cn(
-        "border border-border file:text-foreground placeholder:text-muted-foreground bg-secondary flex h-9 w-full min-w-0 rounded-md px-3 py-1 text-base transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:shadow-none shadow-xs",
+        "border border-border file:text-input-foreground placeholder:text-muted-foreground bg-input flex h-9 w-full min-w-0 rounded-md px-3 py-1 text-base transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm dark:shadow-none shadow-xs",
         "focus-visible:border-border focus-visible:ring-muted focus-visible:ring-[2px]",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
         className
